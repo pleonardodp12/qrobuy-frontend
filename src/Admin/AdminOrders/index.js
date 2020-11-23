6d316d3082bc19a1ef7839d355c3050902bcd965
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-11-23 19:51:14 -0300

    feat: added admin orders listing product page

--- src/Admin/AdminOrders/index.js
+++ src/Admin/AdminOrders/index.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import SearchButton from "../../components/SearchButton";
 
 const AdminOrders = () => {
   return (
-    <div>
-      Admin
-    </div>
-  )
-}
+    <>
+      <div>
+        <SearchButton />
+        <div style={{ display: "flex", justifyContent: 'space-around'}}>
+          <div>
+            <p>Alexandre Santos</p>
+            <p>8FH2U37D</p>
+          </div>
+          <div>
+            <p>289,00 R$</p>
+            <p>16/11/2020</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default AdminOrders
+export default AdminOrders;

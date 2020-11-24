39ac1a727e8ab4947768bd8bbabf627a5750c19d
Paulo Leonardo Dias Pereira <pauloleonardo_dp@outlook.com>
2020-11-23 23:26:42 -0300

    feat: User page: sign-in, cpf mask

--- src/pages/UserSignup/index.js
+++ src/pages/UserSignup/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InputForm from '../../components/InputForm';
+import ButtonConfirm from '../../components/ButtonConfirm';
+import { UserSignupContainer } from './styles';
 
 const UserSignup = () => {
+  const [ cpf, setCpf ] = useState('');
+
+  const cpfMask = value => {
+    return value
+    .replace(/\D/g, '')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+    .replace(/(-\d{2})\d+?$/, '$1')
+  }
+
+  const handleChange = (e) => {
+    setCpf(cpfMask(e.target.value))
+  }
+
   return (
-    <div>
-      
-    </div>
+    <UserSignupContainer>
+      <InputForm labelName="Nome"/>
+      <InputForm labelName="Email" type="email"/>
+      <InputForm
+        labelName="CPF"
+        maxLength='14'
+        value={cpf}
+        onChange={handleChange}
+      />
+      <InputForm labelName="Senha" type="password"/>
+      <InputForm labelName="Digite a senha novamente" type="password"/>
+
+      <ButtonConfirm textButton="Cadastrar" />
+    </UserSignupContainer>
   )
 }
 
--- src/pages/UserSignup/styles.js
+++ src/pages/UserSignup/styles.js
@@ -1,1 +1,13 @@
 import styled from 'styled-components';
+
+export const UserSignupContainer = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  button {
+    margin: 8px 0;
+  }
+`;

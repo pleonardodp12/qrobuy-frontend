4d1af9ed34491aedb109c7170a418d85e9e7dd6c
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-11-27 10:56:51 -0300

    docs: added documentation for the admin api

--- src/services/adminApi.js
+++ src/services/adminApi.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
+/**
+ * Factory for the admin API. Must be used in the following endpoints
+ * [POST] /products
+ * [PATCH] /products/{id}
+ * 
+ * [GET] /orders
+ * [GET] /orders/{id}
+ * [PATCH] /orders/{id}
+ * @param {String} accessToken User access token that is retrieved during signin or siginup. Found in the account state
+ */
 export const makeAdminApi = (accessToken) => {
   return axios.create({
     type: "no-cors",

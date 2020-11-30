7164774a61badb51c6ede760462887ab65e0f6bd
Alexandre Xavier <ale.bxsantos@gmail.com>
2020-11-30 01:12:09 -0300

    feat: added redirection for non admin users

--- src/Admin/AdminOrders/index.js
+++ src/Admin/AdminOrders/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import SearchButton from "../../components/SearchButton";
 import { Container, ListWrapper } from "./styles";
 
-import { mockedOrders } from "./__mocks__/mocked-orders"; // mocked values
 import { OrderDetails } from "./components/order-details";
 import { renderOrdersList } from "./components/order-list";
 import { makeAdminApi } from "../../services/adminApi";
@@ -10,15 +9,18 @@ import { connect } from "react-redux";
 
 const AdminOrders = ({ account }) => {
   const [orders, setOrders] = useState([]);
+  const [error, setError] = useState(null);
   const [details, setDetails] = useState(false);
   const [order, setOrder] = useState({});
 
   const getOrders = async () => {
-    const response = await makeAdminApi(
-      // "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYzBlYzY4YThmM2I3MDAzNDY2MDAzYyIsImlhdCI6MTYwNjcwNTE2Mn0.cz-FOQEbIUX9leuqgKrUsxvK_eATwwA5MunVdQRK33E"
-      account.accessToken
-    ).get("/orders");
-    setOrders(response.data);
+    makeAdminApi(
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmYzBlYzY4YThmM2I3MDAzNDY2MDAzYyIsImlhdCI6MTYwNjcwNTE2Mn0.cz-FOQEbIUX9leuqgKrUsxvK_eATwwA5MunVdQRK33E"
+      // account.accessToken
+    )
+      .get("/orders")
+      .then((response) => setOrders(response.data))
+      .catch((error) => setError(error.response.status));
   };
 
   useEffect(() => {
@@ -29,22 +31,27 @@ const AdminOrders = ({ account }) => {
     setDetails(!details);
     setOrder(order);
   };
-  console.log(orders)
-
-  return (
-    <>
-      <Container>
-        {details ? (
-          <OrderDetails order={order} setDetails={setDetails} />
-        ) : (
-          <>
-            <SearchButton />
-            <ListWrapper>{renderOrdersList(orders, toggleDetails)}</ListWrapper>
-          </>
-        )}
-      </Container>
-    </>
-  );
+  if (!error) {
+    return (
+      <>
+        <Container>
+          {details ? (
+            <OrderDetails order={order} setDetails={setDetails} />
+          ) : (
+            <>
+              <SearchButton />
+              <ListWrapper>
+                {renderOrdersList(orders, toggleDetails)}
+              </ListWrapper>
+            </>
+          )}
+        </Container>
+      </>
+    );
+  } else {
+    window.alert('Não autorizado');
+    window.location.pathname = '/user/sign-in'
+  }
 };
 
 const mapStateToProps = (state) => {

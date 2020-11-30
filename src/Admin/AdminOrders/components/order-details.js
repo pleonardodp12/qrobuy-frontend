1cf3fa2c9728070e61e11e18407fb660fe2df020
Rodrigo Vaz <rodrigodp2014@gmail.com>
2020-11-30 00:37:18 -0300

    feat: add CSS style in Admin OrderDetails

--- src/Admin/AdminOrders/components/order-details.js
+++ src/Admin/AdminOrders/components/order-details.js
@@ -8,20 +8,31 @@ import {
 export const OrderDetails = ({ order, setDetails }) => {
   return (
     <OrderDetailsContainer>
+      <h3>Informações do pedido: {order._id}</h3>
       <TextWrapper>
-        <OrderDetailsText>Número do pedido: {order._id}</OrderDetailsText>
-        <OrderDetailsText>Nome: {order.name}</OrderDetailsText>
-        <OrderDetailsText>CPF: {order.cpf}</OrderDetailsText>
-        <OrderDetailsText>Data: {order.date}</OrderDetailsText>
-        <OrderDetailsText>ID transação: {order.tid}</OrderDetailsText>
         <OrderDetailsText>
-          Valor: {(order.price / 100).toFixed(2).replace(".", ",")}
+          Número do pedido: <span>{order._id}</span>
         </OrderDetailsText>
         <OrderDetailsText>
-          Retirado: {order.retrieved ? "Sim" : "Não"}
+          Nome: <span>{order.name}</span>
+        </OrderDetailsText>
+        <OrderDetailsText>
+          CPF: <span>{order.cpf}</span>
+        </OrderDetailsText>
+        <OrderDetailsText>
+          Data: <span>{order.date}</span>
+        </OrderDetailsText>
+        <OrderDetailsText>
+          ID transação: <span>{order.tid}</span>
+        </OrderDetailsText>
+        <OrderDetailsText>
+          Valor: <span>{(order.price / 100).toFixed(2).replace(".", ",")}</span>
+        </OrderDetailsText>
+        <OrderDetailsText>
+          Retirado: <span>{order.retrieved ? "Sim" : "Não"}</span>
         </OrderDetailsText>
       </TextWrapper>
-      <button onClick={() => setDetails(false)}>close</button>
+      <button onClick={() => setDetails(false)}>Fechar</button>
     </OrderDetailsContainer>
   );
 };

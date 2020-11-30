48afee9d72bdd8037381f855c21bdd66d28c9309
Nicholas-ar <njaf@ic.ufal.br>
2020-11-30 13:26:56 -0300

    fix: commented search button

--- src/components/SearchButton/index.js
+++ src/components/SearchButton/index.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-import { WrapperInput, InputData } from "./styles";
+//import { AiOutlineSearch } from "react-icons/ai";
+//import { WrapperInput, InputData } from "./styles";
 
 const SearchButton = () => {
   return (
+    <div></div>
+    /*
     <WrapperInput>
       <InputData>
         <input type="text" required placeholder="Buscar" />
         <AiOutlineSearch />
       </InputData>
     </WrapperInput>
+    */
   );
 };
 
